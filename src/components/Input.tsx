ec7b57e21c708f359493765c4488dace4014361f
Kristjan Lääne <xonproject@gmail.com>
2024-04-18 15:32:57 +0300

    Building Better Wrapper Components with ComponentPropsWithoutRef

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,15 @@
+import { ComponentPropsWithoutRef } from 'react';
+
 type InputProps = {
   label: string;
   id: string;
-};
+} & ComponentPropsWithoutRef<'input'>;
 
-export const Input = ({ label, id }: InputProps) => {
+export const Input = ({ label, id, ...props }: InputProps) => {
   return (
     <p>
       <label htmlFor={id}>{label}</label>
-      <input id={id} type='text' />
+      <input id={id} {...props} />
     </p>
   );
 };
